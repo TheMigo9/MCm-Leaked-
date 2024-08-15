8393f2b675fe107224d4acb88234ef7266c1b78f
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-08-15 17:11:51 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1134,10 +1134,14 @@ class PlayState extends MusicBeatState
 		if(ClientPrefs.downScroll) {
 			botplayTxt.y = timeBarBG.y - 78;
 		}
-
+                creditTxt.text = "Port by TheMigo"; creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxTextBorderStyle.OUT.LINE,FlxColor , BLACK):
+		creditTxt.scrollFactor.set()
+	        
+		
 		strumLineNotes.cameras = [camHUD];
 		grpNoteSplashes.cameras = [camHUD];
 		notes.cameras = [camHUD];
+	        creditTxt.text.cameras = [camHUD];
 		healthBar.cameras = [camHUD];
 		healthBarBG.cameras = [camHUD];
 		iconP1.cameras = [camHUD];
